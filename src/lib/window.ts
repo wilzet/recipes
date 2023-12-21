e9d0ee77c468ba3ae151b2f7453bb675a53a01e3
Wilmer Z <wilzet02@gmail.com>
2023-12-21 10:52:51 +0100

    Created an app component

--- src/lib/window.ts
+++ src/lib/window.ts
@@ -1,9 +1,6 @@
 import { useState, useEffect } from 'react';
 
 function getWindowDimensions() {
-    if (typeof window === 'undefined') {
-        return { width: 0, height: 0 };
-    }
     const { innerWidth: width, innerHeight: height } = window;
     return { width, height };
 }
@@ -12,7 +9,6 @@ export default function useWindowDimensions() {
     const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
     useEffect(() => {
-        if (typeof window === 'undefined') return;
         const resize = () => setWindowDimensions(getWindowDimensions());
         window.addEventListener('resize', resize);
         return () => window.removeEventListener('resize', resize);
